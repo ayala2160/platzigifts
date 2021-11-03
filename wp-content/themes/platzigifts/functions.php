8cd3111d3a193e286aa786531e31413680b79c78
Elida Ayala Guerrero <ayala2160@gmail.com>
2021-11-02 21:07:55 -0600

    Manejo de librerías

--- wp-content/themes/platzigifts/functions.php
+++ wp-content/themes/platzigifts/functions.php
@@ -6,3 +6,12 @@ function init_template(){
     
 }
 add_action('after_setup_theme','init_template');
+
+function assets(){
+    wp_register('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', '', '4.4.1', 'all');
+    wp_register('montserrat', 'https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap', '', '1.0', 'all');
+
+    wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap', 'montserrat'), '1.0', 'all');
+}
+
+add_action('wp_enqueue_scripts', 'assets');
