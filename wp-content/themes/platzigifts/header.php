c2226dadbf73fd5b7a853e4f8cca6684bc175046
Elida Ayala Guerrero <ayala2160@gmail.com>
2021-11-03 19:07:04 -0600

    Agregando JavaScript al tema

--- wp-content/themes/platzigifts/header.php
+++ wp-content/themes/platzigifts/header.php
@@ -7,3 +7,13 @@
     <?php wp_head() ?>
 </head>
 <body>
+
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="logo">
+            </div>
+        </div>
+    </div>
+</header>
